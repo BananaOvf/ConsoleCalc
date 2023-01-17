7a00c6c2834066afddb36f00787049af5281e65c
BananaOvf <arseniihrynchak06@gmail.com>
2023-01-17 12:36:56 +0300

    First commit

---
+++ Git_Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2, b = 3;
+
+            Console.WriteLine($"{a} + {b} = {a+b}");
+        }
+    }
+}
